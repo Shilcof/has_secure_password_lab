04b12cff567ccc15346132247e1075e3bd336b57
Shilcof <robert.shilcof@gmail.com>
2021-01-11 15:28:10 +0000

    commit tree b522a530561e41a1cb517f746781b3712929b573

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'sessions/login', to: 'sessions#new'
+  post 'sessions/login', to: 'sessions#create'
+  post 'sessions/logout', to: 'sessions#destroy'
+
+  resources :users, only: [:new, :create, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
